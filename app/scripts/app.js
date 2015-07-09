ed80c70a7119498f269bfb607bdb957199553e18
Jen Haines <motochick67@gmail.com>
2015-07-09 11:54:30 -0700

    View and Create Chat Rooms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,4 +10,30 @@
  */
 angular
   .module('blocChatApp', [
-    'ngAnimate', 'ui.router', 'firebase', 'ui.bootstrap']);
+    'ngAnimate', 'ui.router', 'firebase', 'ui.bootstrap'])
+
+  .constant('FIREBASE_URL', 'https://jennifer.firebaseio.com')
+
+  .controller('RoomCtrl', function ($scope, Room, Firebase) {
+      $scope.rooms = Room.all;
+
+      $scope.submitRoom = function(room) {
+          var timestamp = Firebase.ServerValue.TIMESTAMP;
+          room.created = timestamp;
+          Room.create(room).then(function(){
+            $scope.room = {};
+          });
+      };
+  })
+
+  .factory('Room', function(Firebase, $firebaseArray, FIREBASE_URL) {
+    var ref = new Firebase(FIREBASE_URL + '/rooms');
+    var rooms = $firebaseArray(ref);
+
+    return  {
+        all: rooms,
+        create: function(room){
+            return rooms.$add(room);
+        },
+    };
+   });
