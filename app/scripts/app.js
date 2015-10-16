33ea702e6a016959e2380cb9f22c58de65eef6b3
Jen Haines <motochick67@gmail.com>
2015-10-16 12:09:25 -0700

    Heroku Prep

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,22 @@
  *
  * Main module of the application.
  */
+
+ var WebFontConfig = {
+   google: { families: [ 'Architects+Daughter::latin' ] }
+ };
+ (function() {
+   var wf = document.createElement('script');
+   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+   wf.type = 'text/javascript';
+   wf.async = 'true';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(wf, s);
+ })(); 
+
 angular
-  .module('simpleChatApp', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
+  .module('simpleChatApp', ['firebase', 'ui.bootstrap', 'ngCookies'])
 
   .constant('FIREBASE_URL', 'https://jennifer.firebaseio.com')
 
